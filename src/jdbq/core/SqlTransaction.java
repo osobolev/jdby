9970a634bde6b5c25bf6f0c3d80ed1240adf91cf
Oleg Sobolev <sobolev_ov@mail.ru>
2025-11-26 22:54:47 +0100

    Use class instead of interface

--- src/jdbq/core/SqlTransaction.java
+++ src/jdbq/core/SqlTransaction.java
@@ -1,5 +1,24 @@
 package jdbq.core;
 
-// todo: make it class???
-public interface SqlTransaction extends SqlTransactionRaw, RowMapperFactory {
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class SqlTransaction implements SqlTransactionRaw {
+
+    private final RowMapperFactory rowMapperFactory;
+    private final SqlTransactionRaw t;
+
+    public SqlTransaction(RowMapperFactory rowMapperFactory, SqlTransactionRaw t) {
+        this.rowMapperFactory = rowMapperFactory;
+        this.t = t;
+    }
+
+    public <T> RowMapper<T> mapper(Class<T> rowType) {
+        return rowMapperFactory.mapper(rowType);
+    }
+
+    @Override
+    public Connection getConnection() throws SQLException {
+        return t.getConnection();
+    }
 }
