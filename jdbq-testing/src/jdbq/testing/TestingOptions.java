db08c8024473ae69810847d262ab3415254d2adc
Oleg Sobolev <sobolev_ov@mail.ru>
2025-11-27 22:33:35 +0100

    Custom error/warning output

--- jdbq-testing/src/jdbq/testing/TestingOptions.java
+++ jdbq-testing/src/jdbq/testing/TestingOptions.java
@@ -2,7 +2,22 @@ package jdbq.testing;
 
 import jdbq.mapping.CheckColumnCompatibility;
 
+import java.util.function.Consumer;
+
 public final class TestingOptions {
 
     public CheckColumnCompatibility check;
+    public Consumer<String> warn = System.err::println;
+    public Consumer<String> error = message -> {
+        throw new IllegalArgumentException(message);
+    };
+    public boolean checkNamesForPositions = true;
+
+    public void warn(String message) {
+        warn.accept(message);
+    }
+
+    public void error(String message) {
+        error.accept(message);
+    }
 }
