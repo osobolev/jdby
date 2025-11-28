7430209b9f48c47d1a58e4ba9f54fecd9901471f
Oleg Sobolev <sobolev_ov@mail.ru>
2025-11-29 00:04:34 +0100

    Allow null for parameters

--- jdby-core/src/jdby/core/SqlParameter.java
+++ jdby-core/src/jdby/core/SqlParameter.java
@@ -11,19 +11,19 @@ public interface SqlParameter {
 
     void set(PreparedStatement ps, int index) throws SQLException;
 
-    static SqlParameter pInt(int x) {
+    static SqlParameter pInt(Integer x) {
         return new SimpleSqlParameter(x, JDBCType.INTEGER);
     }
 
-    static SqlParameter pLong(long x) {
+    static SqlParameter pLong(Long x) {
         return new SimpleSqlParameter(x, JDBCType.BIGINT);
     }
 
-    static SqlParameter pDouble(double x) {
+    static SqlParameter pDouble(Double x) {
         return new SimpleSqlParameter(x, JDBCType.DOUBLE);
     }
 
-    static SqlParameter pBoolean(boolean x) {
+    static SqlParameter pBoolean(Boolean x) {
         return new SimpleSqlParameter(x, JDBCType.BOOLEAN);
     }
 
