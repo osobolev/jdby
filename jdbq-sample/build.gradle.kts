9c1ff0fa2abcb0738566f56f30c9d775ac2646a6
Oleg Sobolev <sobolev_ov@mail.ru>
2025-11-28 03:13:24 +0100

    Moved back to jdbq-core

--- jdbq-sample/build.gradle.kts
+++ jdbq-sample/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
 
 dependencies {
     implementation(project(":jdbq-core"))
-    implementation(project(":jdbq-dao"))
 
     runtimeOnly("com.h2database:h2:2.2.224")
 }
--- jdbq-testing/build.gradle.kts
+++ jdbq-testing/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
 
 dependencies {
     api(project(":jdbq-core"))
-    api(project(":jdbq-dao"))
 
     "manualRuntimeOnly"("org.postgresql:postgresql:42.7.8")
 }
