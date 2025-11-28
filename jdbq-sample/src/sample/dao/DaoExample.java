eb389818fc6afe8027d8292fcd75eb2edb85cc83
Oleg Sobolev <sobolev_ov@mail.ru>
2025-11-28 02:02:21 +0100

    Dao example

---
+++ jdbq-sample/src/sample/dao/DaoExample.java
@@ -0,0 +1,52 @@
+package sample.dao;
+
+import jdbq.dao.DaoConnection;
+import jdbq.dao.DefaultDaoContext;
+import jdbq.mapping.ColumnNaming;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.List;
+
+@SuppressWarnings({"AutoUnboxing", "UseOfSystemOutOrSystemErr"})
+public class DaoExample {
+
+    public static void main(String[] args) throws SQLException {
+        DefaultDaoContext ctx = new DefaultDaoContext(ColumnNaming.camelCase());
+        try (Connection jdbcConnection = DriverManager.getConnection("jdbc:h2:mem:")) {
+            jdbcConnection.setAutoCommit(false);
+            DaoConnection connection = ctx.withConnection(jdbcConnection);
+
+            SqlDao dao = connection.dao(SqlDao.class);
+
+            dao.createSchema();
+
+            int idNick = dao.insertUser("Nick", LocalDate.of(2000, 12, 8));
+            int idJohn = dao.insertUser("John", LocalDate.of(1990, 12, 8));
+            System.out.printf("Nick: %s, John: %s%n", idNick, idJohn);
+
+            dao.setLastLogin(idJohn, OffsetDateTime.now());
+
+            List<UserRow> allUsers = dao.listAllUsers();
+            System.out.println("All users: " + allUsers);
+
+            List<UserRow> zoomers = dao.listUsersByFilter(null, LocalDate.of(2000, 1, 1), null);
+            System.out.println("Zoomers: " + zoomers);
+
+            UserRow nick = dao.maybeUser(idNick);
+            System.out.println("Nick: " + nick);
+
+            UserRow unknown = dao.maybeUser(-1);
+            System.out.println("Unknown user: " + unknown);
+
+            try {
+                dao.loadUser(-1);
+            } catch (SQLException ex) {
+                System.out.println("User not found!");
+            }
+        }
+    }
+}
---
+++ jdbq-sample/src/sample/dao/UserRow.java
@@ -0,0 +1,14 @@
+package sample.dao;
+
+import jdbq.mapping.SqlName;
+
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+
+public record UserRow(
+    int id,
+    String fullName,
+    @SqlName("dob")
+    LocalDate birthday,
+    OffsetDateTime lastLogin
+) {}
