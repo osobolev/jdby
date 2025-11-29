43962f1de78fdc4e5e455542012545791e8988b2
Oleg Sobolev <sobolev_ov@mail.ru>
2025-11-29 13:15:24 +0100

    Configuration for dependency updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,29 @@
 plugins {
     id("com.github.ben-manes.versions") version "0.53.0"
 }
+
+fun getMajor(version: String, majorDepth: Int): String {
+    var p = -1
+    for (i in 0 until majorDepth) {
+        p = version.indexOf('.', p + 1)
+        if (p < 0) return version
+    }
+    return if (p < 0) "" else version.substring(0, p)
+}
+
+fun getMajorDepth(mod: ModuleComponentIdentifier): Int {
+    return 1
+}
+
+tasks.withType(com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask::class).configureEach {
+    resolutionStrategy {
+        componentSelection {
+            all(Action<com.github.benmanes.gradle.versions.updates.resolutionstrategy.ComponentSelectionWithCurrent> {
+                val majorDepth = getMajorDepth(candidate)
+                if (getMajor(candidate.version, majorDepth) != getMajor(currentVersion, majorDepth)) {
+                    reject("Major update")
+                }
+            })
+        }
+    }
+}
