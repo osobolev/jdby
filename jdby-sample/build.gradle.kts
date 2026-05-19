f1512042beb104cf291ef486bd38c0a98c5c6c8e
Oleg Sobolev <sobolev_ov@mail.ru>
2026-05-20 00:18:18 +0200

    Dependencies update

--- jdby-sample/build.gradle.kts
+++ jdby-sample/build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
     runtimeOnly("com.h2database:h2:2.4.240")
 
     testImplementation("io.github.osobolev.jdby:jdby-testing:1.3")
-    testImplementation("org.junit.jupiter:junit-jupiter:6.0.3")
+    testImplementation("org.junit.jupiter:junit-jupiter:6.1.0")
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
